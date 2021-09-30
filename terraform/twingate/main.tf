f3b72e56d702572452b53fa7b1b80b1e28378016
keithhubner <keith@hubner.co.uk>
2021-09-30 10:14:53 +0100

    Added new variables and removed unrequired code

--- terraform/twingate/main.tf
+++ terraform/twingate/main.tf
@@ -1,4 +1,4 @@
-# Configure the Civo Provider
+# Set the Providers
 terraform {
   required_providers {
     civo = {
@@ -16,17 +16,18 @@ terraform {
     }
 }
 
-
+# Provider tokens
 provider "civo" {
   token = var.civo_token
 }
 
 provider "twingate" {
   api_token = var.twingate_token
-  network   = "keith"
+  network   = var.network
 }
 
-data "civo_instances_size" "medium" {
+# Get instance sizes from Civo and order by ram
+data "civo_instances_size" "instances" {
     filter {
         key = "type"
         values = ["kubernetes"]
@@ -37,64 +38,52 @@ data "civo_instances_size" "medium" {
         direction = "asc"
     }
 }
-
-#output "instances" {
-#  value       = data.civo_instances_size.small.sizes
-#  description = "The Civo Instances."
-#}
-
+# New cluster with the name "my-cluster", with 3 nodes and taking the 3rd instance size from the list generated above
 resource "civo_kubernetes_cluster" "my-cluster" {
     name = "my-cluster"
     applications = "Ghost:5GB"
     num_target_nodes = 3
-    target_nodes_size = element(data.civo_instances_size.medium.sizes, 2).name
+    target_nodes_size = element(data.civo_instances_size.instances.sizes, 2).name
 }
 
+# Return the name of the newly generated cluster
 data "civo_kubernetes_cluster" "my-cluster" {
     name = civo_kubernetes_cluster.my-cluster.name
 }
 
-#output "kubernetes_cluster_output" {
-#  value = data.civo_kubernetes_cluster.my-cluster.kubeconfig
-#}
-
+# Store the cluster config in the ignore directory
 resource "local_file" "private_key" {
     content  = data.civo_kubernetes_cluster.my-cluster.kubeconfig
     filename = "ignore/civo.conf"
 }
 
-
+# Setup a new remote network on Twingate
 resource "twingate_remote_network" "civo_network" {
   name = "civo_remote_network"
 }
 
-output "twingate_remote_network" {
-  value = data.civo_kubernetes_cluster.my-cluster.kubeconfig
-}
-
+# Create the new twingate connector
 resource "twingate_connector" "civo_connector" {
   remote_network_id = twingate_remote_network.civo_network.id
 }
 
+# Create the tokens for the new connector
 resource "twingate_connector_tokens" "civo_connector_tokens" {
   connector_id = twingate_connector.civo_connector.id
 }
 
-#data "twingate_connector_tokens" "civo_connector" {
- #   name = "civo_connector"
+
+#output "twingate_access_token_output" {
+#  value = twingate_remote_network.civo_network.id
+
 #}
 
-output "twingate_access_token_output" {
-  value = twingate_remote_network.civo_network.id
+#output "twingate_refresh_token_output" {
+#  value = twingate_connector_tokens.civo_connector_tokens.refresh_token 
+#  sensitive = true
+#}
 
-}
-
-output "twingate_refresh_token_output" {
-  value = twingate_connector_tokens.civo_connector_tokens.refresh_token 
-  sensitive = true
-}
-
-
+# Deploy the connector to the new k8s cluster via helm
 provider "helm" {
   kubernetes {
     config_path = local_file.private_key.filename
@@ -103,14 +92,13 @@ provider "helm" {
 
 resource "helm_release" "twingate_connector" {
   name       = "connector"
-
   repository = "https://twingate.github.io/helm-charts"
   chart      = "connector"
   version = "0.1.8"
 
   set {
     name  = "connector.network"
-    value = "keith"
+    value = var.network
   }
   set {
     name  = "connector.accessToken"
@@ -122,10 +110,10 @@ resource "helm_release" "twingate_connector" {
   }    
 }
 
-
-
+# Add a new resouce to twingate
 resource "twingate_resource" "resource" {
   name = "ghost"
   address = "ghost-blog.ghost.svc.cluster.local"
   remote_network_id = twingate_remote_network.civo_network.id
+  group_ids = [var.groupID]
 }
