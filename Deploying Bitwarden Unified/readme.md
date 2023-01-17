8ef7624a63635d47de2a52b14fe65c6f300d4602
keithhubner <keith@hubner.co.uk>
2023-01-17 12:16:06 +0000

    updates

--- Deploying Bitwarden Unified/readme.md
+++ Deploying Bitwarden Unified/readme.md
@@ -14,18 +14,72 @@ tfvars file api key
 
 main.tf
 
+> The kubeconfig should automatically be saved and merged
+
+Switch to the context
+
+```
+kubectx CLUSTER_NAME
+```
+
+### Create Namespace
+
+```
+kubectl create ns bitwarden
+```
+
 ## Creating the database
 
-> Need to create via the CLI at the moment
-```
-civo database create bitwarden --size g3.db.xsmall
+> Need to create via the CLI at the moment until terraform provider update.
+
+> Check you are in the same region as the cluster
 
 ```
+civo database create bitwarden --size g3.db.xsmall --firewall demo-firewall-db
+
+```
+
+Wait until the db is created:
+
+```
+civo db ls
+```
+
+Get the connection information:
+
+```
+civo db credentials bitwarden
+```
+
+> You may want to create a spoecific bitwarden mysql user and give it permissions to only the bitwarden db
 
 Creating the db connection secret
 
-db_conn_bitwarden.yaml
+Create the secrets for db conneciton, key and ID
+```
+kubectl create secret generic bitwarden \
+    --from-literal=connstring='server=[db_server_ip];port=3306;database=bitwarden;user=root;password=[your_password]' \
+    --from-literal=installationID='[your_installation_KEY]]' \
+    --from-literal=installationKEY='[your_installation_key]' -n bitwarden
 ```
 
+Create the config map:
 
 ```
+kubectl apply -f configmap_bitwarden.yaml
+```
+
+Apply the deployment:
+
+```
+kubectl apply -f deployment_bitwarden.yaml
+```
+
+Connect to via proxy forward to test:
+
+```
+kubectl port-forward svc/bitwarden 8080:80
+```
+
+You should see the bitwarden login page where you can create an account
+
--- Deploying Bitwarden Unified/terraform/main.tf
+++ Deploying Bitwarden Unified/terraform/main.tf
@@ -9,6 +9,10 @@ terraform {
   }
 }
 
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
 # Configure the Civo Provider
 provider "civo" {
   token = var.civo_token
@@ -32,21 +36,43 @@ resource "civo_firewall" "demo-firewall-k8s" {
     name = "demo-firewall-k8s"
 }
 
-# Create a firewall for the database
+# Create a firewall for the database and an access rule for the cluster to access the db
 resource "civo_firewall" "demo-firewall-db" {
     name = "demo-firewall-db"
+    create_default_rules = false
+    ingress_rule {
+        label = "k8s access"
+        protocol = "tcp"
+        port_range = "3306"
+        cidr = [format("%s/%s",civo_kubernetes_cluster.demo-cluster.master_ip,"32")]
+        action = "allow"
+    }
+        ingress_rule {
+        label = "personal access"
+        protocol = "tcp"
+        port_range = "3306"
+        cidr = ["${chomp(data.http.myip.response_body)}/32"]
+        action = "allow"
+    }
+    
 }
 
-
 # Create a cluster
-resource "civo_kubernetes_cluster" "my-cluster" {
-    name = "demo-cluster"
-    # applications = ""
+resource "civo_kubernetes_cluster" "demo-cluster" {
+    # name = "" # Specify a name if you like, I enjoy seeing the random ones! 
+    # applications = "" 
     firewall_id = civo_firewall.demo-firewall-k8s.id
     pools {
         label = "demo-pool" // Optional
         size = element(data.civo_size.medium.sizes, 0).name
         node_count = 3
     }
+     provisioner "local-exec" {
+    command = "civo k3s config ${civo_kubernetes_cluster.demo-cluster.name} --save --merge"
+    
+  }
 }
 
+output "cluster_name" {
+  value = civo_kubernetes_cluster.demo-cluster.name
+}
